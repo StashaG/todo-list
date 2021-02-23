22e8a2fc7dbc5f4e35d98ccce398450dd029b325
Stacey Graham <stacey.graham1106@gmail.com>
2021-02-23 13:16:03 -0500

    added components Form and TodoList and updated App.js to render Form component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import "./App.css"
+import Form from "./components/Form";
 
 function App() {
   return (
@@ -7,6 +8,7 @@ function App() {
       <header>
       <h1>Stacey's Todo List</h1>
       </header>
+      <Form />
     </div>
   )
 }
