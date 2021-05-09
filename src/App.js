d479f7f512c601de6e51857ed35cc6cdfb571422
Stacey Graham <stacey.graham1106@gmail.com>
2021-05-08 23:33:16 -0400

    updated App with inputText state and added an onChange event Handler

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./App.css"
 import Form from "./components/Form";
 import TodoList from "./components/TodoList";
 
 
 function App() {
+  const [inputText, setInputText] = useState("");
   return (
     <div className="App">
       <header>
       <h1>Stacey's Todo List</h1>
       </header>
-      <Form />
+      <Form setInputText={setInputText}/>
       <TodoList />
     </div>
   )
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 
-const Form = () => {
+const Form = ({ setInputText }) => {
+  const inputTextHandler = (e) => {
+      console.log(e.target.value);
+      setInputText(e.target.value);
+  };
   return (
     <form>
-      <input type="text" className="todo-input" />
+      <input onChange={inputTextHandler} type="text" className="todo-input" />
       <button className="todo-button" type="submit">
         <i className="fas fa-plus-square"></i>
       </button>
